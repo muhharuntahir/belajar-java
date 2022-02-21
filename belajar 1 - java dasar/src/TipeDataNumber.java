3f7ff9233a3ef4ff62a8825eea60ec6a666efcc2
Muhammad Harun <muhharuntahir@gmail.com>
2022-02-21 17:24:43 +0800

    Mempelajari tipe data number lainnya

--- belajar 1 - java dasar/src/TipeDataNumber.java
+++ belajar 1 - java dasar/src/TipeDataNumber.java
@@ -1,7 +1,21 @@
 public class TipeDataNumber {
     public static void main(String[] args) {
         byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInt = 10000000;
+        long iniLong = 1000000000;
+        long iniLong2 = 1000000000L;
 
-        System.out.println(iniByte);
+        float iniFloat = 10.10F;
+        double iniDouble = 10.10;
+
+        int decimalInt = 34;
+        int hexDecimal = 0xFFFFFF;
+        int binaryDecimal = 0b10101010;
+
+        int amount = 1_000_000_000;
+
+
+        System.out.println(amount);
     }
 }
