027bd2dbb5a1337dd3d2929493fbf6a5dde64fa8
Muhammad Harun <muhharuntahir@gmail.com>
2022-02-21 17:33:39 +0800

    Mempelajari Tipe Data Character

---
+++ belajar 1 - java dasar/src/TipeDataChar.java
@@ -0,0 +1,16 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+
+        char h = 'H';
+        char a = 'A';
+        char r = 'R';
+        char u = 'U';
+        char n = 'N';
+
+        System.out.print(h);
+        System.out.print(a);
+        System.out.print(r);
+        System.out.print(u);
+        System.out.print(n);
+    }
+}
